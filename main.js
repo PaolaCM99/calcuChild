cd6133fc49e29e0d36ae38e8987a38427049ede2
Leidy Paola <paoalm1@hotmail.com>
2022-03-25 18:59:07 -0500

    functions to sum

--- main.js
+++ main.js
@@ -1,49 +1,65 @@
 
-var operacion = document.getElementById("operacion")
+document.getElementById("back").addEventListener("click", Atras)
+document.getElementById("operacion").addEventListener("click", mostrarOperacionesMatematicas)
 
-var select = operacion.addEventListener("click", function () {
-	if (document.getElementById("select-operation").style.display = "none") {
-		document.getElementById("select-operation").style.display = "flex";
-		document.getElementById("select-option").style.display = "none";
-	} else {
-		document.getElementById("select-operation").style.display = "none";
-	}
-})
+let con = 10;
+let suma = 0;
+let numberTwo = "";
 
-var element = document.getElementById("back")
+function Suma() {
+	do {
+		let btn = document.getElementById('submit-suma');
+		let input = document.getElementById("input-suma");
+		btn.addEventListener("click", () => {
+			let numberOne = input.value;
+			suma = suma + parseFloat(numberOne);
+			document.getElementById('display-suma').textContent = numberTwo + "+" + numberOne;
+			document.getElementById('display-result').textContent = suma;
+			document.getElementById("input-suma").value = suma;
+			numberTwo = suma;
+		})
 
-var option = element.addEventListener("click", function () {
+	} while (con < 3);
+}
+
+function Resta() {
+	console.log("value of Resta")
+
+};
+function Multiplicacion() {
+	let valueOne = 4;
+	let ValueTwo = 2;
+	let total = valueOne * ValueTwo;
+};
+function division() {
+	console.log("value of division")
+
+}
+Suma();
+Multiplicacion();
+
+function Atras() {
 	if (document.getElementById("select-operation").style.display = "flex") {
 		document.getElementById("select-operation").style.display = "none";
 		document.getElementById("select-option").style.display = "flex";
 	} else {
 		document.getElementById("select-operation").style.display = "flex";
 	}
-})
-
-//option
-//switch
-//funcion matematicas
-//whil
-
-function Suma(){
-	print("value of suma")
-
-};
-function Resta(){
-	print("value of Resta")
-
-};
-function Multiplicacion(){
-	console.log("value of multiplicacion")
-let	valueOne=4;
-let	ValueTwo=2;
-let total= valueOne*ValueTwo;
-console.log(total);
-
-};
-function division() {
-	print("value of division")
-
 }
-Multiplicacion();
+function mostrarOperacionesMatematicas() {
+	if (document.getElementById("select-operation").style.display = "none") {
+		document.getElementById("select-operation").style.display = "flex";
+		document.getElementById("select-option").style.display = "none";
+	} else {
+		document.getElementById("select-operation").style.display = "none";
+	}
+}
+
+function Limpiar() {
+	document.getElementById("input-suma").value = 0;
+	suma = 0;
+	numberTwo = 0;
+	numberOne = 0;
+}
+
+document.getElementById('clear').addEventListener("click", Limpiar)
