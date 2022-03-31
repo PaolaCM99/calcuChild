05f02331d78f4d4b5c3d4a20a6d6f9693309b074
Leidy Paola <paoalm1@hotmail.com>
2022-03-30 21:24:13 -0500

    Validation numbers NaN

--- buttons.js
+++ buttons.js
@@ -17,3 +17,26 @@ export  function buttons(){
 	document.getElementById("identificacion").addEventListener("click", mostrarIdentificacion)
 	document.getElementById("backIdentificacion").addEventListener("click", atrasIdentificacion)
 }
+
+export function Limpiar(acomulador, numeroDos, numeroUno) {
+	acomulador = 0;
+	numeroDos = 0;
+	numeroUno = 0;
+	document.getElementById("input-suma").value = "";
+	document.getElementById('display-result').textContent = 0;
+	document.getElementById('display-suma').textContent = 0;
+
+	document.getElementById("input-resta").value = "";
+	document.getElementById('display-result').textContent = 0;
+	document.getElementById('display-resta').textContent = 0;
+	document.getElementById('notas').textContent = "";
+	document.getElementById('display-division').textContent = "";
+	document.getElementById('display-result-multiplication').textContent = "";
+	document.getElementById('notas-multiplicacion').textContent = "";
+	document.getElementById("input-identificacion-uno").value = 0;
+	document.getElementById("input-identificacion-dos").value = 0;
+	document.getElementById('display-multiplicacion').textContent = "";
+	document.getElementById('display-result-numbers').textContent = 0;
+	document.getElementById('display-identificacion').textContent = 0;
+
+}
--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 
-import { buttons } from "./buttons.js"
+import { buttons, Limpiar } from "./buttons.js"
 
 buttons();
 
 let con = 10;
-let acomulador = 0;
+let acomulador = 1;
 let numeroDos = "";
 let numeroUno = "";
 
-//Los valores se resetean y quedan en 0, lo mejor seria dejarlos vacios(null)
+
 function Suma() {
 	do {
 		let btn = document.getElementById('submit-suma');
 		let input = document.getElementById("input-suma");
 		btn.addEventListener("click", () => {
-			let numeroUno = input.value;
+			let numeroUno = input.value + "+";
 			acomulador = acomulador + parseFloat(numeroUno);
-			document.getElementById('display-suma').textContent = numeroDos + "" + numeroUno;
+			document.getElementById('display-suma').textContent = numeroUno + numeroDos;
 			document.getElementById('display-result').textContent = acomulador;
-			document.getElementById("input-suma").value = acomulador;
+			document.getElementById("input-suma").value = "";
 			numeroDos = acomulador;
 		})
 
@@ -30,11 +30,11 @@ function Resta() {
 		let btn = document.getElementById('submit-resta');
 		let input = document.getElementById("input-resta");
 		btn.addEventListener("click", () => {
-			let numeroUno = input.value;
+			let numeroUno = input.value + "-";
 			acomulador = parseFloat(numeroUno) - parseFloat(acomulador);
-			document.getElementById('display-resta').textContent = numeroDos + "-" + numeroUno;
+			document.getElementById('display-resta').textContent = numeroUno + numeroDos;
 			document.getElementById('display-result-subtraction').textContent = acomulador;
-			document.getElementById("input-resta").value = acomulador;
+			document.getElementById("input-resta").value = "";
 			numeroDos = acomulador;
 		})
 
@@ -44,14 +44,32 @@ function Resta() {
 function Multiplicacion() {
 	do {
 		let btn = document.getElementById('submit-multiplicacion');
-		let input = document.getElementById("input-multiplicacion");
+		let inputUno = document.getElementById("input-multiplicacion-uno");
+		let inputDos = document.getElementById("input-multiplicacion-dos");
+		let nota = document.getElementById('notas-multiplicacion');
+		let displayResult = document.getElementById('display-result-multiplication')
 		btn.addEventListener("click", () => {
-			let numeroUno = input.value;
-			acomulador = parseFloat(numeroUno) * parseFloat(acomulador);
-			document.getElementById('display-multiplicacion').textContent = numeroDos + "*" + numeroUno;
-			document.getElementById('display-result-subtraction').textContent = acomulador;
-			document.getElementById("input-multiplicacion").value = acomulador;
-			numeroDos = acomulador;
+			let numeroUno = inputUno.value + "*";
+			let numeroDos = inputDos.value;
+			if (parseFloat(numeroUno) == 0 || parseFloat(numeroDos) == 0) {
+				nota.textContent = "Cualquier numero multiplicado por 0 da como resultado 0";
+			}
+
+			let resultado = parseFloat(numeroUno) * parseFloat(numeroDos);
+			if (isNaN(resultado)) {
+				nota.textContent = "Ingresa un numero"
+				nota.style.color = "rgb(248, 38, 255)"
+				displayResult.textContent = "Ingresa un numero"
+				displayResult.style.fontSize = "15px"
+			} else {
+				document.getElementById('display-multiplicacion').textContent = numeroUno + numeroDos;
+				displayResult.textContent = resultado;
+				nota.style.color = "black"
+				inputUno.value = "";
+				inputDos.value = "";
+			};
+
+
 		})
 
 	} while (con < 3);
@@ -60,14 +78,37 @@ function division() {
 
 	do {
 		let btn = document.getElementById('submit-division');
-		let input = document.getElementById("input-division");
+		let inputUno = document.getElementById("input-division-uno");
+		let inputDos = document.getElementById("input-division-dos");
+		let nota= document.getElementById('notas');
+		let displayResult= document.getElementById('display-result-division');
 		btn.addEventListener("click", () => {
-			let numeroUno = input.value;
-			acomulador = parseFloat(numeroUno) / parseFloat(acomulador);
-			document.getElementById('display-division').textContent = numeroDos + "/" + numeroUno;
-			document.getElementById('display-result-subtraction').textContent = acomulador;
-			document.getElementById("input-division").value = acomulador;
-			numeroDos = acomulador;
+			let numeroUno = inputUno.value;
+			let numeroDos = inputDos.value;
+			let resultado = parseFloat(numeroUno) / parseFloat(numeroDos);
+		
+
+			if (isNaN(resultado)) {
+				nota.textContent = "Ingresa un numero"
+				nota.style.color = "rgb(248, 38, 255)"
+				displayResult.textContent = "Ingresa un numero"
+				displayResult.style.fontSize = "15px"
+			}else{
+				nota.textContent = ""
+				nota.style.color = "black"
+				if (numeroUno == 0) {
+					nota.textContent = "El valor es 0, ya que cualquier numero en el dividendo es igual a cero";
+				} else if (numeroDos == 0) {
+					nota.textContent = "Si su divisor es 0, el resultado se considera indefinido";
+				}
+
+				document.getElementById('display-division').textContent = numeroUno + "/" + numeroDos;
+				document.getElementById('display-result-division').textContent = resultado;
+				inputUno.value = "";
+				inputDos.value = "";
+			}
+
+			
 		})
 
 	} while (con < 3);
@@ -79,6 +120,7 @@ function identificacion() {
 		let btn = document.getElementById('submit-identificacion');
 		let inputUno = document.getElementById("input-identificacion-uno");
 		let inputDos = document.getElementById("input-identificacion-dos");
+
 		btn.addEventListener("click", () => {
 			let numeroUno = parseFloat(inputUno.value);
 			let numeroDos = parseFloat(inputDos.value);
@@ -101,24 +143,16 @@ function identificacion() {
 }
 
 
-function Limpiar() {
-	acomulador = 0;
-	numeroDos = 0;
-	numeroUno = 0;
-	document.getElementById("input-suma").value = 0;
-	document.getElementById('display-result').textContent = 0;
-	document.getElementById('display-suma').textContent = 0;
 
-	document.getElementById("input-resta").value = 0;
-	document.getElementById('display-result').textContent = 0;
-	document.getElementById('display-resta').textContent = 0;
-
-}
-
+Limpiar(acomulador,numeroDos, numeroUno);
 Resta()
 Suma();
 Multiplicacion();
 identificacion();
 division()
+
 document.getElementById('clear').addEventListener("click", Limpiar)
 document.getElementById('clearResta').addEventListener("click", Limpiar)
+document.getElementById('clearDivision').addEventListener("click", Limpiar)
+document.getElementById('clearMultiplicacion').addEventListener("click", Limpiar)
+document.getElementById('clearIdentificacion').addEventListener("click", Limpiar)
